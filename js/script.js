0838e24c916cdbc3836f73391273354d8652bc9f
Mitch Thompson <thomitchell@gmail.com>
2016-05-10 11:14:08 -0700

    Adds bootstrap css to header & nav

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $(".box").not(".box-1").hide();
+    
+    $("nav a.box-1").click(function(event) {
+        $("nav a").not(".box-1").removeAttr( 'style' );
+        $(this).css('text-decoration', 'underline');
+        $(".box").not(".box-1").hide(1000);
+        $(".box-1").show(1000);
+    });
+    
+    $("nav a.box-2").click(function(event) {  
+        $("nav a").not(".box-2").removeAttr( 'style' );
+        $(this).css('text-decoration', 'underline');
+        $(".box").not(".box-2").hide(1000);
+        $(".box-2").show(1000);
+    });
+    
+     $("nav a.box-3").click(function(event) { 
+        $("nav a").not(".box-3").removeAttr( 'style' );
+        $(this).css('text-decoration', 'underline');
+        $(".box").not(".box-3").hide(1000);
+        $(".box-3").show(1000);
+    });
+    
+    $("nav a.box-4").click(function(event) {
+        $("nav a").not(".box-4").removeAttr( 'style' );
+        $(this).css('text-decoration', 'underline');     
+        $(".box").not(".box-4").hide(1000);
+        $(".box-4").show(1000);
+    });
+  
+    
+    
+
+    
+    
+    
+    
+    //add css to nav on mouseover then removes on mouseleave
+//    $("nav a").mouseover(function(){
+//        $(this).css(
+//            'color', 'blue');
+//    });
+//    $("nav a").mouseleave(function(){
+//        $(this).removeAttr( 'style' );
+//    });
+});
+
--- script/script.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $(".box").not(".box-1").hide();
-});
